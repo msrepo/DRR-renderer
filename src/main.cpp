22256158a28e5f140fbf982fa80a729ac15a766e
Mahesh Shakya <maheshshakya.01@gmail.com>
2020-05-22 11:06:06 +0545

    changed input param parsing

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,12 @@ int main(int argc, const char **argv) {
 
   // we only load mhd file
   ArgumentParser parser("Parse CT file path");
+
   parser.add_argument()
-      .names({"-d", "--dir"})
-      .description("file directory")
+      .names({"-f", "--file"})
+      .description("file path")
       .required(true);
-  parser.add_argument()
-      .names({"-r", "--raw"})
-      .description("raw file name")
-      .required(true);
-  parser.add_argument()
-      .names({"-i", "--info"})
-      .description("info (mhd) file")
-      .required(true);
-  parser.add_argument()
-      .names({"-x", "--resize"})
-      .description("resize factor")
-      .required(false);
+
   parser.add_argument()
       .names({"-t", "--type"})
       .description("file type")
@@ -38,10 +28,9 @@ int main(int argc, const char **argv) {
     parser.print_help();
     return 0;
   }
-  std::string CT_folder = "../data/";
-  std::string filenameCT = CT_folder + "FullHead.raw";
+  std::string CT_folder = "data/";
+  std::string raw_filename = CT_folder + "FullHead.raw";
   std::string info_filename = CT_folder + "FullHead.mhd";
-  float resize_factor = 1.0f;
   const std::string DEFAULT_FILETYPE = "MHD";
   std::string filetype = DEFAULT_FILETYPE;
 
@@ -49,21 +38,14 @@ int main(int argc, const char **argv) {
     filetype = parser.get<std::string>("t");
   }
 
-  if (parser.exists("d")) {
-    CT_folder = parser.get<std::string>("d");
-  }
-  if (parser.exists("r")) {
-    filenameCT = CT_folder + parser.get<std::string>("r");
-  }
-  if (parser.exists("i")) {
-    info_filename = CT_folder + parser.get<std::string>("i");
-  }
-  if (parser.exists("x")) {
-    resize_factor = parser.get<float>("x");
+  if (parser.exists("f")) {
+    info_filename = parser.get<std::string>("f");
+    info_filename = info_filename.substr(0, info_filename.rfind(".")) + ".mhd";
+    raw_filename = info_filename.substr(0, info_filename.rfind(".")) + ".raw";
   }
 
   const auto startct = chrono::system_clock::now();
-  drrgene.load_CT(filenameCT, info_filename);
+  drrgene.load_CT(raw_filename, info_filename);
   const auto stopct = chrono::system_clock::now();
   const auto duratct =
       chrono::duration_cast<chrono::milliseconds>(stopct - startct).count();
@@ -71,18 +53,19 @@ int main(int argc, const char **argv) {
 
   cv::Mat color;
   const auto start = chrono::system_clock::now();
-  drrgene.raytracegpu(color, resize_factor);
+  drrgene.raytracegpu(color);
   const auto stop = chrono::system_clock::now();
   const auto durat =
       chrono::duration_cast<chrono::milliseconds>(stop - start).count();
   std::cout << "DRR created in " << durat << " ms" << std::endl;
   cv::namedWindow("DRR", CV_WINDOW_KEEPRATIO);
   cv::imshow("DRR", color);
-  std::string filename = parser.get<std::string>("r");
-  filename = filename.substr(0, filename.find(".")) + ".png";
-  std::cout << "Writing DRR to file " << filename << std::endl;
+
+  std::string out_filename =
+      info_filename.substr(0, info_filename.rfind(".")) + ".png";
+  std::cout << "Writing DRR to file " << out_filename << std::endl;
   std::cout << "Matrix size:" << color.size() << std::endl;
-  cv::imwrite(filename, color);
+  cv::imwrite(out_filename, color);
   cv::waitKey(0);
   return 0;
 }
